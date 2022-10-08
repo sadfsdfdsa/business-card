edb1b0b14d8139e7de0168ec6bebb35061431ffe
Artem Shuvaev <shuvaevlol@gmail.com>
2022-10-08 22:58:13 +0600

    fix: safari background image

--- src/App.vue
+++ src/App.vue
@@ -115,6 +115,7 @@ const scrollToBegin = () => {
 <style>
 .custom-img {
   background-image: linear-gradient(rgba(0, 0, 0, 0.527), rgba(0, 0, 0, 0.5)),
+    url('https://images.unsplash.com/photo-1454496522488-7a8e488e8606?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1955&q=80'),
     url(./assets/high.avif), url(./assets/low.avif);
 }
 </style>
